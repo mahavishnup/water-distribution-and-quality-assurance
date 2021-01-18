b5ee935af2a45a916b537b7647976b70bb7a7ec4
Mahavishnu P <52984889+mahavishnup@users.noreply.github.com>
2021-01-18 22:40:43 +0530

    meta tag command

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,9 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+{{--        <meta http-equiv="X-UA-Compatible" content="ie=edge">--}}
+{{--        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">--}}
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Water Distribution - @yield('title')</title>
