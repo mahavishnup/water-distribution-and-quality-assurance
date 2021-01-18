ba28c895fae7735b1285754e2cc50fcc79952026
Mahavishnu P <52984889+mahavishnup@users.noreply.github.com>
2021-01-18 23:03:04 +0530

    meta tag command

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,14 +2,19 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="UTF-8">
-{{--        <meta http-equiv="X-UA-Compatible" content="ie=edge">--}}
-{{--        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">--}}
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta http-equiv="Content-Security-Policy" content="default-src 'self'">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="theme-color" content="#000000" />
+        <meta
+            name="description"
+            content="Water Distribution and Quality Assurance"
+        />
+        <!-- Favicon -->
+        <link href="{{ '/images/logo/water-distribution.png' }}" rel="icon" type="image/png">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Water Distribution - @yield('title')</title>
-        <!-- Favicon -->
-        <link href="{{ '/images/logo/water-distribution.png' }}" rel="icon" type="image/png">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
         <!-- Extra details for Live View on GitHub Pages -->
