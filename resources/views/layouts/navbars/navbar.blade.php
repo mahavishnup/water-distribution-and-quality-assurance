274af1d4db96e00a2e0204fce258eb39ae78e6a8
Mahavishnu P <52984889+mahavishnup@users.noreply.github.com>
2021-01-19 11:47:08 +0530

    @ add path

--- resources/views/layouts/navbars/navbar.blade.php
+++ resources/views/layouts/navbars/navbar.blade.php
@@ -1,9 +1,9 @@
-@if(\App\Models\User::find(Auth::user()->id)->is_admin == '0')
-    @include('layouts.navbars.navs.auth')
-@endif
-@if(\App\Models\User::find(Auth::user()->id)->is_admin == '1')
-    @include('layouts.navbars.navs.admin')
-@endif
 @guest()
     @include('layouts.navbars.navs.guest')
 @endguest
+@if(@\App\Models\User::find(Auth::user()->id)->is_admin == '0')
+    @include('layouts.navbars.navs.auth')
+@endif
+@if(@\App\Models\User::find(Auth::user()->id)->is_admin == '1')
+    @include('layouts.navbars.navs.admin')
+@endif
