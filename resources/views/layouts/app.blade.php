bb5586483aa50880a3b02f12e21af83f72d15cca
Mahavishnu P <52984889+mahavishnup@users.noreply.github.com>
2021-01-19 07:53:59 +0530

    app.blade.php update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <meta http-equiv="Content-Security-Policy" content="default-src 'self'">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="theme-color" content="#000000" />
         <meta
